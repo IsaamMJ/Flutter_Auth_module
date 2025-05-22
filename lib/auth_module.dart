7e6fbd72450b36af04a3cd72cc7818336b0105c8
IsaamMJ <136082478+IsaamMJ@users.noreply.github.com>
2025-05-22 23:38:57 +0530

    added config file 02

--- lib/auth_module.dart
+++ lib/auth_module.dart
@@ -4,6 +4,6 @@ export 'presentation/pages/login_page.dart';
 export 'presentation/pages/signup_page.dart';
 export 'presentation/bindings/login_binding.dart';
 export 'presentation/bindings/signup_binding.dart';
-export 'routes/auth_pages.dart';
-export 'routes/app_routes.dart';
-export 'auth_module_config.dart'; // ✅ exports AuthModule.init
+export 'routes/auth_pages.dart';     // ✅ GetX pages
+export 'routes/app_routes.dart';     // ✅ contains AuthRoutes
+export 'package:auth_module/auth_module_config.dart';      // ✅ contains AuthModule.init()
